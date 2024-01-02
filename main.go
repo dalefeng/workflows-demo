8a90e7b3c12a8992105ca39d622d81dee3602516
likfees <likfees@gmail.com>
2024-01-02 22:47:07 +0800

    complete

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	jsoniter "github.com/json-iterator/go"
+	"os"
 )
 
 func main() {
 	m := map[string]string{
-		"hello": "world12111",
+		"hello": "world",
 	}
+	baseURL := os.Getenv("BASE_URL")
+	fmt.Println("BASE_URL:", baseURL)
 	marshal, _ := jsoniter.Marshal(m)
 	fmt.Println(string(marshal))
 }
